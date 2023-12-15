38e519bfa8fc13bf0feb489efeb20d2c5b5742c7
chenww <chenww@qq.com>
2023-12-15 15:00:34 +0800

    调整UI样式

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -9,20 +9,20 @@ Page({
       //   url: '/pages/basic/mobilenet/index'
       // },
       {
-        title: '表情识别',
-        img: 'https://i.postimg.cc/434GMrbX/image.jpg',
+        title: '微表情识别',
+        img: 'https://th.bing.com/th/id/R.988f488fbdf56ea83a8aebdbfc9600fd?rik=mPgIyG2%2fVeVDFQ&riu=http%3a%2f%2fwww.cyberlink.com%2fstat%2ftechnology%2fimg%2f1200x630_face.jpg&ehk=QudBwtE0XMNyhpb7wl5OUMQlnAAyQBHKfT55QdaNm%2bs%3d&risl=&pid=ImgRaw&r=0',
         url: '/pages/basic/face/index'
       },
       {
-        title: 'AI视频',
-        img: 'https://i.postimg.cc/d0MgYGj4/AI.jpg',
-        url: '/pages/basic/posenet/index'
+        title: 'AI大事记',
+        img: 'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/final_keyword_header.width-1200.format-webp.webp',
+        url: '/pages/posts/post'
       },
       {
-        title: 'AI大事记',
-        img: 'https://i.postimg.cc/65gmrCgt/AI-2.jpg',
-        url: '/pages/posts/post'
-      }
+        title: 'AI微视频',
+        img: 'https://image.jiqizhixin.com/uploads/editor/62cecc07-39a4-4a50-be4e-c9294dd3540e/640.gif',
+        url: ''
+      },
     ]
   },
 
